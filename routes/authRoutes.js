c4b2eec380d857d11ec12667df2b9cc7515e367f
Himanshi Rangnani <98536981+himanshi-28@users.noreply.github.com>
2023-06-17 06:08:28 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,22 +1,15 @@
-/* the code imports the necessary modules. 
-The express module is imported to create a router object, 
-and the registerController, loginController, and logoutController functions are imported from the authController module.*/
-
 const express=require('express');
 const { registerController, loginController, logoutController } = require('../controllers/authController');
 
 //router object
-/*This line creates a router object using the express.Router() function. 
-The router object allows you to define routes and their associated handlers.*/
 const router=express.Router();
 
 //routes
-//REGISTER
 
+//REGISTER
 router.post('/register', registerController);
 //LOGIN
 router.post('/login', loginController);
-
 //LOGOUT
 router.post('/logout', logoutController);
 
